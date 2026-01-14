f35b7c190dca60c0481a02210b958fbb3818bd47
Matheus Plaza <plazachave.mp7@gmail.com>
2026-01-14 11:41:58 -0300

    feat: secure application architecture using Angular Guards for route protection, JWT validation, and automated session expiration handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { SignInPage } from './pages/not-logged/sing-in/sign-in';
 import { SignUpPage } from './pages/not-logged/sing-up/sign-up';
 import { Home } from './pages/logged/home/home';
-import { CreateMovie } from './pages/logged/create-movie/create-movie';
+import { CreateMovie } from './pages/logged/create-movie/create-movie'
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: SignInPage },
   { path: 'cadastre-se', component: SignUpPage },
-  { path: 'home', component: Home},
-  {path: 'novo-filme', component: CreateMovie}
+  { path: 'home', component: Home, canActivate: [authGuard]},
+  { path: 'novo-filme', component: CreateMovie, canActivate: [authGuard]},
+
+  { path: '', redirectTo: '/login',  pathMatch: 'full' },
+  { path: '**', redirectTo: '/login'}
 ];
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { LoginRequest, LoginResponse, UserRequest, UserResponse } from '../models/auth.models';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -25,5 +26,34 @@ export class AuthService {
 
   getToken(): string | null {
     return localStorage.getItem('token');
+  }
+
+  isTokenValid(): boolean {
+    const token = this.getToken();
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const decoded: any = jwtDecode(token);
+
+      const expirationDate = decoded.exp;
+      const currentDate = Math.floor(Date.now() / 1000);
+
+      if (expirationDate < currentDate) {
+        console.warn('Token expirado!');
+        return false;
+      }
+
+      return true;
+
+    } catch (error) {
+      return false;
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('token');
   }
 }
